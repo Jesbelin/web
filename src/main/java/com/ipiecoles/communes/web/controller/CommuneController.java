6df887d2ab11dcbefa0d7f1f2432805850393fc0
Jesbelin <j.esbelin@gmail.com>
2021-06-30 12:54:32 +0200

    Chevrons marchent. ràf: mettre à jour le numéro de page de la liste

--- src/main/java/com/ipiecoles/communes/web/controller/CommuneController.java
+++ src/main/java/com/ipiecoles/communes/web/controller/CommuneController.java
@@ -3,26 +3,96 @@ package com.ipiecoles.communes.web.controller;
 import com.ipiecoles.communes.web.model.Commune;
 import com.ipiecoles.communes.web.repository.CommuneRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Comparator;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Controller
 public class CommuneController {
+    public static final Double DEGRE_LAT_KM = 111d;
+    public static final Double DEGRE_LONG_KM = 77d;
+
     @Autowired
     private CommuneRepository communeRepository;
 
-    @GetMapping(value = "/communes/{codeInsee}")
+    @GetMapping("/communes/{codeInsee}")
     public String getCommune(
             @PathVariable String codeInsee,
+            @RequestParam(defaultValue = "10") Integer perimetre,
             final ModelMap model)
     {
         Optional<Commune> commune = communeRepository.findById(codeInsee);
+        if(commune.isEmpty()){
+            //Gère une exception
+        }
+        //Récupérer les communes proches de celle-ci
         model.put("commune", commune.get());
+        model.put("communesProches", this.findCommunesProches(commune.get(), perimetre));
+        model.put("newCommune", false);
         return "detail";
+    }
+
+    @GetMapping("/communes/new")
+    public String newCommune(
+            final ModelMap model)
+    {
+        model.put("commune",new Commune());
+        model.put("newCommune", true);
+        return "detail";
+    }
+
+    @PostMapping(value ="/communes", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public String saveNewCommune(
+            Commune commune,
+            final ModelMap model)
+    {
+        // Ajouter un certain nombre de contrôles...
+        commune = communeRepository.save(commune);
+        model.put("commune", commune);
+        return "detail";
+    }
+
+    @PostMapping(value ="/communes/{codeInsee}", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public String saveExistingCommune(
+            Commune commune,
+            @PathVariable String codeInsee,
+            final ModelMap model)
+    {
+        // Ajouter un certain nombre de contrôles...
+        commune = communeRepository.save(commune);
+        model.put("commune", commune);
+        return "detail";
+    }
+
+    /**
+     * Récupère une liste des communes dans un périmètre autour d'une commune
+     * @param commune La commune sur laquelle porte la recherche
+     * @param perimetreEnKm Le périmètre de recherche en kilomètre
+     * @return La liste des communes triées de la plus proche à la plus lointaine
+     */
+    private List<Commune> findCommunesProches(Commune commune, Integer perimetreEnKm) {
+        Double latMin, latMax, longMin, longMax, degreLat, degreLong;
+        //1 degré latitude = 111km, 1 degré longitude = 77km
+        degreLat = perimetreEnKm/DEGRE_LAT_KM;
+        degreLong = perimetreEnKm/DEGRE_LONG_KM;
+        latMin = commune.getLatitude() - degreLat;
+        latMax = commune.getLatitude() + degreLat;
+        longMin = commune.getLongitude() - degreLong;
+        longMax = commune.getLongitude() + degreLong;
+        List<Commune> communesProches = communeRepository.findByLatitudeBetweenAndLongitudeBetween(latMin, latMax, longMin, longMax);
+        ;
+        return communesProches.stream().
+                filter(commune1 -> !commune1.getNom().equals(commune.getNom()) && commune1.getDistance(commune.getLatitude(), commune.getLongitude()) <= perimetreEnKm).
+                sorted(Comparator.comparing(o -> o.getDistance(commune.getLatitude(), commune.getLongitude()))).
+                collect(Collectors.toList());
     }
 }
--- src/main/java/com/ipiecoles/communes/web/controller/IndexController.java
+++ src/main/java/com/ipiecoles/communes/web/controller/IndexController.java
@@ -1,11 +1,17 @@
 package com.ipiecoles.communes.web.controller;
 
+import com.ipiecoles.communes.web.model.Commune;
 import com.ipiecoles.communes.web.repository.CommuneRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Arrays;
 
 @Controller
 public class IndexController {
@@ -15,17 +21,34 @@ public class IndexController {
 
     @GetMapping(value = "/")
     public String index(
-            @RequestParam(value="page", defaultValue = "0") Integer page,
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
             @RequestParam(defaultValue = "10") Integer size,
             @RequestParam(defaultValue = "codeInsee") String sortProperty,
             @RequestParam(defaultValue = "ASC") String sortDirection,
-            final ModelMap model){
-        model.put("nbCommunes", communeRepository.count());
-        //Affichage des communes de 1 à 10 => page = 0 et size = 10
-        //Affichage des communes de 11 à 20 => page = 1 et size = 10
-        //Affichage des communes de 41 à 60 => page = 2 et size = 20
-        model.put("start", 1);//A remplacer par la valeur dynamique
-        model.put("end", 10);//A remplacer par la valeur dynamique
-        return "list"; //Chemin du template (sans.html) à partir du dossier templates
+            @RequestParam(required = false) String search, //false pour dire que le paramètre search n'est pas obligatoire dans l'url
+            final ModelMap model) {
+
+        //Constituer un PageRequest
+        PageRequest pageRequest = PageRequest.of(page, size, Sort.Direction.fromString(sortDirection), sortProperty);
+        Page<Commune> communes;
+        if(search == null || search.isEmpty()){
+            //Appeler findAll si search est null
+            communes = communeRepository.findAll(pageRequest);
+        } else {
+            //Appeler findByNomContainingIgnoreCase si search n'est pas null
+            communes = communeRepository.findByNomContainingIgnoreCase(search, pageRequest);
+        }
+        model.put("communes", communes);
+        model.put("nbCommunes", communes.getTotalElements());
+        model.put("pagesSizes", Arrays.asList("5", "10", "20", "50", "100"));
+
+        // Affichage des communes de 1 à 10 => page =0 et size = 10
+        // Affichage des communes de 11 à 20 => page =1 et size = 10
+        // Affichage des communes de 41 à 60 => page =2 et size = 20
+
+        model.put("start", 1); // A remplacer par la valeur dynamique
+        model.put("end", 10); // A remplacer par la valeur dynamique
+        model.put("page", page);
+        return "list"; //Chemin du template (sans .html) à partir du dossier templates
     }
 }
